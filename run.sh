3569aacf1383eb7d7062f63c15fa80f05fa3e6ca
hank418 <dennis790626@gmail.com>
2018-05-18 15:26:14 +0800

    image in private dockerhub

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -d -p 1238:8080 -v ${PWD}/rent.db:/vol/rent.db -it countingrent
+docker run -d -p 1238:8080 -v ${PWD}/rent.db:/vol/rent.db -it hank418/private:countingrent
